af8a9d70ad4040ad5e25db74b5a3962742d7ee45
Kevin Coyner <rustybear@gmail.com>
2017-06-12 20:13:07 -0400

    doodle: Completed course.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(age, job) {
+    super(age, job)
+    this.age = 10
+    this.job = 'make honey'
+    this.honeyPot = 0
+  }
+  makeHoney () {
+    return this.honeyPot++;
+  }
+  giveHoney() {
+    return this.honeyPot--;
+  }
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,15 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(age, job, color, canFly) {
+    super(age, job, color, canFly)
+    this.age = 40
+    this.job = 'gamble'
+    this.color = 'grey'
+    this.canFly = false
+  }
+  forage () {
+    return 'I am too old, let me play cards instead';
+  }
+  gamble (treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
